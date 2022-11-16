65f7833cfa50cd5157ac72a67dc4b0f7b8574f1c
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-11-16 13:42:35 -0800

    from last video

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express');
-const { port } = require('./config.json');
+const { port, token, appId } = require('./config.json');
 const path = require('path');
+const { fetch } = require('undici');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname + '/public')));
 
-app.get('/', (request, response) => {
-  return response.sendFile('index.html', {root: '.'});
+app.get('/config/:id', async (request, response) => {
+  let configData = await getGuildCommands(request.params.id);
+  console.log(configData);
+  response.render('config', {
+    data: configData
+  });
 });
+
+const getGuildCommands = async (id) => {
+  console.log(id);
+  const response = await fetch(`https://discord.com/api/v10/applications/${appId}/commands`, {
+    headers: {
+      authorization: `Bot ${token}`,
+    },
+  });
+  return response.json();
+};
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
